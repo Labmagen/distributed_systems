13c67a884c646722db228783ae664b3a474a3456
Niels-Martin Paulsen <stu244979@mail.uni-kiel.de>
2026-01-03 00:32:56 +0100

    Implement vector clock

--- Lab 03 Template/vector_clock.py
+++ Lab 03 Template/vector_clock.py
@@ -3,31 +3,62 @@ from typing import Self
 class VectorClock():
     def __init__(self, n=1, entries=None):
         # TODO: Init vector clock with num and integer entries, you might want to create a copy of the entries
-        pass
+        if entries != None:
+            self.clock = list(entries)
+        else:
+            self.clock = [0] * n
 
     # TODO: Increment the respective clock index when an event occurs on server i
     def increment(self, i):
-        pass
+        if i < len(self.clock):
+            self.clock[i] += 1 
+        else:
+            while len(self.clock) <= i:
+                self.clock.append(0)
+            self.clock[i] += 1
 
     # TODO: Update our own entries based on the other clock
     def update(self, other: Self):
-        pass
+        i = 0
+        while i < max(len(self.clock), len(other.clock)):
+            if len(self.clock) <= i:
+                self.clock.append(other.clock[i])
+            elif len(other.clock) <= i:
+                pass
+            else:
+                if self.clock[i] < other.clock[i]:
+                    self.clock[i] = other.clock[i]
+            i += 1
 
     # Todo: Implement me
     def from_list(entries : list) -> Self:
-        return VectorClock()
+        return VectorClock(entries=entries)
 
     # Todo: Implement me
     def to_list(self) -> list:
-        return []
+        return list(self.clock)
 
     # Todo: Implement me
     def is_parallel(self, other):
-        pass
+        return not (self < other) and not (other < self) and self.clock != other.clock
 
     # Todo: Implement me, check if our clock is strictly smaller than the other one!
     def __lt__(self, other):
-        return False
+        equal = True
+        i = 0
+        while i <  max(len(self.clock), len(other.clock)):
+            if i >= len(self.clock):
+                if other.clock[i] > 0:
+                    equal = False
+            elif i >= len(other.clock):
+                if self.clock[i] > 0:
+                    return False
+            elif self.clock[i] > other.clock[i]:
+                return False
+            elif self.clock[i] < other.clock[i]:
+                equal = False
+            i += 1
+        return not equal
 
     # a simple copy function for now
     def copy(self : Self) -> Self:
